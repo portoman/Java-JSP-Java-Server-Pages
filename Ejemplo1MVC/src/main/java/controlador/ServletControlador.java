3fb14a2db06e7ed567d67008f42d5b9a20465644
portobuj <portbuj@yahoo.es>
2021-05-20 13:42:43 +0200

    Update 20210520

---
+++ Ejemplo1MVC/src/main/java/controlador/ServletControlador.java
@@ -0,0 +1,31 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import modelo.Rectangulo;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //1. Procesamos parametros
+        
+        //2. Creamos los JavaBeans
+        Rectangulo rec=new Rectangulo(3,6);
+        
+        //3. Agregamos el JavaBEan a algun alcande(page, request, session o application)
+        request.setAttribute("mensaje","Saludos desde Servlet");
+        
+                        
+        HttpSession sesion=request.getSession();
+        sesion.setAttribute("rectangulo", rec);
+        
+        //4. Redireccionar a la vista seleccionada
+        RequestDispatcher rd=request.getRequestDispatcher("vista/desplegarVariables.jsp");
+        rd.forward(request, response);
+    }
+}
---
+++ Ejemplo1MVC/src/main/webapp/vista/desplegarVariables.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Despliegue de Variables</title>
+    </head>
+    <body>
+        <h1>Despliegue de Variables</h1>
+        Variable en alcance request: ${mensaje}
+        
+    </body>
+</html>
